ea5070df5a6caf896996e33a57f37a277041384e
John Moshakis <john@moshakis.com>
2015-10-13 23:07:04 -0400

    switch to proper structure

--- ReachAbility.pas
+++ ReachAbility.pas
@@ -8,6 +8,46 @@ uses
 type
 
 
+//struct sockaddr_in {
+//  __uint8_t  sin_len;
+//  sa_family_t  sin_family;
+//  in_port_t  sin_port;
+//  struct  in_addr sin_addr;
+//  char    sin_zero[8];
+//};
+//typedef unsigned char    __uint8_t;
+//typedef __uint8_t    sa_family_t;
+//typedef  __uint16_t    in_port_t;
+//typedef  unsigned short    __uint16_t;
+//struct in_addr {
+//  in_addr_t s_addr;
+//};
+//typedef  __uint32_t  in_addr_t;  /* base type for internet address */
+//typedef unsigned int    __uint32_t;
+
+
+  __uint8_t = UInt8;
+  sa_family_t = __uint8_t;
+  __uint16_t = UInt16; 
+  in_port_t = __uint16_t;
+  __uint32_t = UInt32;
+  in_addr_t = __uint32_t;
+
+  in_addr = public record
+  public
+    s_addr:in_addr_t;
+  end;
+
+  sockaddr_in = public record
+  public
+    sin_len:__uint8_t;
+    sin_family:sa_family_t;
+    sin_port:in_port_t;
+    sin_addr:in_addr;
+    sin_zero:array of Char := new Char[8];
+  end;
+
+
   NetworkStatus = public enum (NotReachable, ReachableViaWiFi, ReachableViaWWAN); 
 
   Reachability = public class
@@ -43,7 +83,7 @@ type
     method currentReachabilityStatus:NetworkStatus;
     
     class method reachabilityWithHostName(hostName:NSString):Reachability;
-    class method reachabilityWithAddress(hostAddress:__struct_sockaddr):Reachability;
+    class method reachabilityWithAddress(hostAddress:^sockaddr_in):Reachability;
     class method reachabilityForInternetConnection:Reachability;
     class method reachabilityForLocalWiFi:Reachability;
     
@@ -117,9 +157,9 @@ class method Reachability.ReachabilityCallback (target:SCNetworkReachabilityRef;
 begin
   using autoreleasepool do
   begin
-    //var noteObject:^Reachability := (__bridge ^Reachability) info;
+    var noteObject := bridge<Reachability>(info);
     // Post a notification to notify the client that the network reachability changed. 
-    //NSNotificationCenter.defaultCenter.postNotificationName(kReachabilityChangedNotification) object(noteObject);
+    NSNotificationCenter.defaultCenter.postNotificationName(kReachabilityChangedNotification) object(noteObject);
   end;
 end;
 
@@ -159,35 +199,34 @@ end;
 
 class method Reachability.reachabilityForInternetConnection: Reachability;
 begin
-  var zeroAddress:__struct_sockaddr;
+  var zeroAddress:sockaddr_in;
   bzero(@zeroAddress, sizeOf(zeroAddress));
-  zeroAddress.sa_len := sizeOf(zeroAddress);
-  zeroAddress.sa_family := AF_INET;
-  exit reachabilityWithAddress(zeroAddress);  
+  zeroAddress.sin_len := sizeOf(zeroAddress);
+  zeroAddress.sin_family := AF_INET;
+  exit reachabilityWithAddress(@zeroAddress);  
 end;
 
 class method Reachability.reachabilityForLocalWiFi: Reachability;
 begin
-  var localWifiAddress:__struct_sockaddr;
+  var localWifiAddress:sockaddr_in;
   bzero(@localWifiAddress, sizeOf(localWifiAddress));
-  localWifiAddress.sa_len := sizeOf(localWifiAddress);
-  localWifiAddress.sa_family := AF_INET;
-  // IN_LINKLOCALNETNUM is defined in <netinet/in.h> as 169.254.0.0
-  // TODO..
-  //localWifiAddress.sin_addr.s_addr = htonl(IN_LINKLOCALNETNUM);
-  var retVal : Reachability := reachabilityWithAddress(localWifiAddress);
+  localWifiAddress.sin_len := sizeOf(localWifiAddress);
+  localWifiAddress.sin_family := AF_INET;
+  localWifiAddress.sin_addr.s_addr := htonl(IN_LINKLOCALNETNUM);
+  var retVal : Reachability := reachabilityWithAddress(@localWifiAddress);
   if(assigned(retVal))then
   begin
     retVal.localWiFiRef := true;
   end;
   exit retVal;
   
+  
 end;
 
-class method Reachability.reachabilityWithAddress(hostAddress: __struct_sockaddr): Reachability;
+class method Reachability.reachabilityWithAddress(hostAddress: ^sockaddr_in): Reachability;
 begin
   
-  var reachability:SCNetworkReachabilityRef  := SCNetworkReachabilityCreateWithAddress(kCFAllocatorDefault, @hostAddress);
+  var reachability:SCNetworkReachabilityRef  := SCNetworkReachabilityCreateWithAddress(kCFAllocatorDefault, ^__struct_sockaddr(hostAddress));
   var retVal : Reachability := nil;
   if(assigned(reachability))then
   begin
